7992a6711dc0981d4cd7f15f6f796dc6b4ba15a5
yogiatwork <babayogisingh@gmail.com>
2019-04-08 23:49:04 +0530

    updating gitcode

--- pipline/scheduled/gitcheckout.groovy
+++ pipline/scheduled/gitcheckout.groovy
@@ -1,9 +1,23 @@
-node {
+def fetch_code() {
+    stage('git checkout'){
+        deleteDir()
+        git checkout
+    }
+}
+
+def checkvars() {
+    withEnv([ "ENV=uat"]){
+        stage('get the environemnt'){
+            echo env.ENV
+        }
+    }
+}
+node('master') {
     stage('message'){
         dir('C:/Work'){
             bat 'dir'
         }
-       isUnix()    
-       echo 'Hello World'
-    } 
+    }
+    fetch_code
+    checkvars 
 }
